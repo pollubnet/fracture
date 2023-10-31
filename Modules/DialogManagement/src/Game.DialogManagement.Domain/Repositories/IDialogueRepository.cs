b3479bfcda1ea0297800df5a0d3070d6d608afae
naomiEve <0xlunaric@gmail.com>
2023-10-31 10:37:56 +0100

    feat: add the IDialogueRepository interface.

---
+++ Modules/DialogManagement/src/Game.DialogManagement.Domain/Repositories/IDialogueRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Game.DialogManagement.Domain.Data.Entities;
+
+namespace Game.DialogManagement.Domain.Repositories
+{
+    /// <summary>
+    /// The interface implemented by anything that wants to store dialogues.
+    /// </summary>
+    public interface IDialogueRepository
+    {
+        /// <summary>
+        /// Gets a dialogue from the repository given the npc id and the player id.
+        /// </summary>
+        /// <param name="npcId">The id of the NPC we're conversing with.</param>
+        /// <param name="playerId">The id of the player conversing.</param>
+        /// <returns>Either the dialogue, or nothing if one doesn't exist.</returns>
+        Dialogue? Get(Guid npcId, Guid playerId);
+
+        /// <summary>
+        /// Creates a dialogue in the repository.
+        /// </summary>
+        /// <param name="dialogue">The dialogue to create.</param>
+        void Create(Dialogue dialogue);
+
+        /// <summary>
+        /// Updates the dialogue in the repository.
+        /// </summary>
+        /// <param name="dialogue">The dialogue to be updated.</param>
+        void Update(Dialogue dialogue);
+    }
+}
