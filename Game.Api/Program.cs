507be2ff66dcbf7a22a111c9588ab5cc6ecb892a
Ktosi33 <54289137+Ktosi33@users.noreply.github.com>
2023-11-06 22:03:55 +0100

    feat: Generate DependencyInjection and inject it

--- Game.Api/Program.cs
+++ Game.Api/Program.cs
@@ -1,11 +1,11 @@
 using Game.AccountManagement.Api;
+using Game.DialogManagement.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
 // Add services to the container.
-builder.Services.AddAccountMangementModule();
+builder.Services.AddAccountMangementModule().AddDialogManagementModule();
+
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
