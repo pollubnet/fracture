480059d169dae8ad44378275863170c20e1c058f
naomiEve <0xlunaric@gmail.com>
2023-11-06 20:01:25 +0100

    feat: add the generic ai provider interface.
    it will provide the ability to generate prompt responses for any sub-module
    requiring AI generation.

---
+++ Shared/Game.Shared.External/Providers/Ai/IAiProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Game.Shared.External.Providers.Ai
+{
+    /// <summary>
+    /// Provides the ability to generate the response to a prompt.
+    /// </summary>
+    public interface IAiProvider
+    {
+        /// <summary>
+        /// Generates the response to a prompt.
+        /// </summary>
+        /// <param name="prompt">The context.</param>
+        /// <returns>The resulting generated response to said prompt.</returns>
+        Task<string> Generate(AiGenerationContext prompt);
+    }
+}
