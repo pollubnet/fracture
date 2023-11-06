fdd0bbcdf98cffecaba75dbc6c0c0da8cfca8d36
naomiEve <0xlunaric@gmail.com>
2023-11-06 19:42:41 +0100

    refactor: change DialogueContext into an entity.
    also add the message as a separate ValueObject entity for later usage.

--- Modules/DialogManagement/src/Game.DialogManagement.Domain/Data/Entities/Dialogue.cs
+++ Modules/DialogManagement/src/Game.DialogManagement.Domain/Data/Entities/Dialogue.cs
@@ -25,12 +25,25 @@ namespace Game.DialogManagement.Domain.Data.Entities
         public DialogueContext Context { get; private set; } = null!;
 
         /// <summary>
+        /// Creates a new dialogue for a given player and npc.
+        /// </summary>
+        /// <param name="playerId">The player's id.</param>
+        /// <param name="npcId">The npc's id.</param>
+        public Dialogue(Guid playerId, Guid npcId)
+        {
+            PlayerId = playerId;
+            NpcId = npcId;
+
+            Context = new();
+        }
+
+        /// <summary>
         /// Adds a message to this dialogue's context backlog.
         /// </summary>
         /// <param name="message">The message.</param>
         public void AddMessageToBacklog(string message)
         {
-            // TODO
+            Context.MessageLog.Add(new(message));
         }
     }
 }
---
+++ Modules/DialogManagement/src/Game.DialogManagement.Domain/Data/ValueObjects/DialogueMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Game.Shared.Core.Data;
+
+namespace Game.DialogManagement.Domain.Data.ValueObjects
+{
+    /// <summary>
+    /// A single dialogue message.
+    /// </summary>
+    /// <param name="Message">The message.</param>
+    public record DialogueMessage(string Message) : ValueObject { }
+}
