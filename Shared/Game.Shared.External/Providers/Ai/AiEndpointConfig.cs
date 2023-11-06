355a8ce97e7ca889f994b0cc4991030edf1db345
naomiEve <0xlunaric@gmail.com>
2023-11-07 00:18:51 +0100

    feat: add the configuration class for an ai endpoint.
    this will hold the endpoint, as i doubt we'll need more than this...

---
+++ Shared/Game.Shared.External/Providers/Ai/AiEndpointConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Game.Shared.External.Providers.Ai
+{
+    /// <summary>
+    /// The config for the AI provider's endpoint.
+    /// </summary>
+    public class AiEndpointConfig
+    {
+        /// <summary>
+        /// The endpoint's URL.
+        /// </summary>
+        public string EndpointUrl { get; set; } = null!;
+    }
+}
