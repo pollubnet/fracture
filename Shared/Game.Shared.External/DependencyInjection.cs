c40c70ae869591afe05753a503b754e013d5c0f6
naomiEve <0xlunaric@gmail.com>
2023-11-06 23:50:13 +0100

    feat: add the dependency injection helper for shared.external

---
+++ Shared/Game.Shared.External/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Game.Shared.External
+{
+    /// <summary>
+    /// Extensions for the dependency injection container.
+    /// </summary>
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Adds the shared external modules into the dependency injection container.
+        /// </summary>
+        public static IServiceCollection AddSharedExternal(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
