77aac43b0228df6f7cb13bde95f9f2c03526a3ab
Ktosi33 <54289137+Ktosi33@users.noreply.github.com>
2023-11-06 22:03:55 +0100

    feat: Generate DependencyInjection and inject it

---
+++ Modules/DialogManagement/src/Game.DialogManagement.Api/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Game.DialogManagement.Application;
+using Game.DialogManagement.Infrastracture;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.DialogManagement.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDialogManagementModule(this IServiceCollection services)
+        {
+            services.AddApplication().AddInfrastructure();
+
+            return services;
+        }
+    }
+}
