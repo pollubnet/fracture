933f10c87279b91d3399dde7529192074dfa17d6
naomiEve <0xlunaric@gmail.com>
2023-11-06 19:42:41 +0100

    refactor: change DialogueContext into an entity.
    also add the message as a separate ValueObject entity for later usage.

---
+++ Modules/DialogManagement/src/Game.DialogManagement.Domain/Data/Entities/DialogueContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Game.Shared.Core.Data;
+
+namespace Game.DialogManagement.Domain.Data.ValueObjects
+{
+    /// <summary>
+    /// The dialogue context, containing all of the information neccessary to
+    /// actually converse.
+    /// </summary>
+    public class DialogueContext : Entity
+    {
+        /// <summary>
+        /// The name of the NPC we're conversing with.
+        /// </summary>
+        public string NpcName { get; init; } = null!;
+
+        /// <summary>
+        /// The NPC's backstory.
+        /// </summary>
+        public string NpcStory { get; init; } = null!;
+
+        /// <summary>
+        /// The name of the player that's conversing with the player.
+        /// </summary>
+        public string PlayerName { get; init; } = null!;
+
+        /// <summary>
+        /// The message log for this dialogue context.
+        /// </summary>
+        public List<DialogueMessage> MessageLog { get; private set; } = null!;
+
+        /// <summary>
+        /// Constructs a default dialogue context.
+        /// </summary>
+        public DialogueContext()
+        {
+            MessageLog = new();
+        }
+    }
+}
