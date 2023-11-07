7c6b6917b323cf57deadd610e264d6494fa209ef
naomiEve <0xlunaric@gmail.com>
2023-11-07 15:44:58 +0100

    refactor: async all the things in IDialogueRepository.

--- Modules/DialogManagement/src/Game.DialogManagement.Domain/Repositories/IDialogueRepository.cs
+++ Modules/DialogManagement/src/Game.DialogManagement.Domain/Repositories/IDialogueRepository.cs
@@ -14,18 +14,20 @@ namespace Game.DialogManagement.Domain.Repositories
         /// <param name="npcId">The id of the NPC we're conversing with.</param>
         /// <param name="playerId">The id of the player conversing.</param>
         /// <returns>Either the dialogue, or nothing if one doesn't exist.</returns>
-        Dialogue? Get(Guid npcId, Guid playerId);
+        Task<Dialogue?> Get(Guid npcId, Guid playerId);
 
         /// <summary>
         /// Creates a dialogue in the repository.
         /// </summary>
         /// <param name="dialogue">The dialogue to create.</param>
-        void Create(Dialogue dialogue);
+        /// <returns>Whether the creation of the dialogue has succeeded.</returns>
+        Task<bool> Create(Dialogue dialogue);
 
         /// <summary>
         /// Updates the dialogue in the repository.
         /// </summary>
         /// <param name="dialogue">The dialogue to be updated.</param>
-        void Update(Dialogue dialogue);
+        /// <returns>Whether the update has succeeded.</returns>
+        Task<bool> Update(Dialogue dialogue);
     }
 }
