827b98b116a5d39531580c8b1668f3fdf75777eb
naomiEve <0xlunaric@gmail.com>
2023-11-09 17:14:39 +0100

    feat: add the CreateDialogue command.

---
+++ Modules/DialogManagement/src/Game.DialogManagement.Application/Handlers/Commands/CreateDialogueHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using MediatR;
+using Game.DialogManagement.Application.Contracts.Commands;
+using Game.DialogManagement.Domain.Data.Entities;
+using Game.DialogManagement.Domain.Data.ValueObjects;
+using Game.DialogManagement.Domain.Repositories;
+
+namespace Game.DialogManagement.Application.Handlers.Commands
+{
+    /// <summary>
+    /// The handler for the CreateDialogue command.
+    /// </summary>
+    public class CreateDialogueHandler : IRequestHandler<CreateDialogueCommand, Dialogue>
+    {
+        /// <summary>
+        /// The repository holding all the dialogues.
+        /// </summary>
+        private readonly IDialogueRepository _dialogueRepository;
+
+        /// <summary>
+        /// Creates a new CreateDialogue handler with a given dialogue repo.
+        /// </summary>
+        /// <param name="dialogueRepository">The dialogue repo.</param>
+        public CreateDialogueHandler(IDialogueRepository dialogueRepository)
+        {
+            _dialogueRepository = dialogueRepository;
+        }
+
+        /// <summary>
+        /// Creates a dialogue from the parameters supplied by the command.
+        /// </summary>
+        /// <param name="request">The creation command.</param>
+        /// <returns>The created dialogue entity.</returns>
+        private static Dialogue CreateDialogue(CreateDialogueCommand request)
+        {
+            var context = new DialogueContext()
+            {
+                NpcName = request.NpcName,
+                NpcStory = request.NpcStory,
+                PlayerName = request.PlayerName
+            };
+            var dialogue = new Dialogue(request.PlayerId, request.NpcId, context);
+
+            return dialogue;
+        }
+
+        /// <inheritdoc/>
+        public async Task<Dialogue> Handle(
+            CreateDialogueCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var dialogue = CreateDialogue(request);
+            if (!await _dialogueRepository.Create(dialogue))
+            {
+                // TODO: Handle creation failures.
+            }
+
+            return dialogue;
+        }
+    }
+}
