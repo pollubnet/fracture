2a8af06c859eea7c9d6349597a14acd8bf075f41
naomiEve <0xlunaric@gmail.com>
2023-11-09 17:14:09 +0100

    feat: add the GetDialogue query.

---
+++ Modules/DialogManagement/src/Game.DialogManagement.Application/Contracts/Queries/GetDialogueQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Game.DialogManagement.Domain.Data.Entities;
+using MediatR;
+
+namespace Game.DialogManagement.Application.Contracts.Queries
+{
+    /// <summary>
+    /// A query for fetching a dialogue by its player id and npc id.
+    /// </summary>
+    /// <param name="PlayerId">The player's id.</param>
+    /// <param name="NpcId">The npc's id.</param>
+    public record GetDialogueQuery(Guid PlayerId, Guid NpcId) : IRequest<Dialogue?> { }
+}
