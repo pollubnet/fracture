75017c3533651323eb284c00923fdd53a9a5e820
naomiEve <0xlunaric@gmail.com>
2023-11-09 17:15:46 +0100

    feat: add the DialogueChat command.

---
+++ Modules/DialogManagement/src/Game.DialogManagement.Application/Contracts/Commands/DialogueChatCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MediatR;
+using Game.DialogManagement.Domain.Data.Entities;
+
+namespace Game.DialogManagement.Application.Contracts.Commands
+{
+    /// <summary>
+    /// A command for generating the next message by the NPC from a dialogue.
+    /// </summary>
+    /// <param name="Dialogue">The dialogue we're currently in.</param>
+    /// <param name="UserMessage">The message the user has sent.</param>
+    public record DialogueChatCommand(Dialogue Dialogue, string UserMessage) : IRequest<string> { }
+}
---
+++ Modules/DialogManagement/src/Game.DialogManagement.Application/Handlers/Commands/DialogueChatHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MediatR;
+using Game.DialogManagement.Application.Contracts.Commands;
+using Game.DialogManagement.Domain.Providers;
+
+namespace Game.DialogManagement.Application.Handlers.Commands
+{
+    /// <summary>
+    /// The handler for the DialogueChat command.
+    /// </summary>
+    public class DialogueChatHandler : IRequestHandler<DialogueChatCommand, string>
+    {
+        /// <summary>
+        /// The AI provider for the chat functionality.
+        /// </summary>
+        private readonly IAiChatProvider _aiChatProvider;
+
+        /// <summary>
+        /// Constructs a new dialogue chat handler for a given AI provider.
+        /// </summary>
+        /// <param name="aiChatProvider">The AI provider for the chat.</param>
+        public DialogueChatHandler(IAiChatProvider aiChatProvider)
+        {
+            _aiChatProvider = aiChatProvider;
+        }
+
+        /// <inheritdoc/>
+        public async Task<string> Handle(
+            DialogueChatCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            var response = await _aiChatProvider.Chat(
+                request.Dialogue.Context,
+                request.UserMessage
+            );
+            return response;
+        }
+    }
+}
