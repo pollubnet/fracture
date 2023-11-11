9932fa0ebac79e74d8ad6ff4200e7019b57fda39
Ktosi33 <54289137+Ktosi33@users.noreply.github.com>
2023-11-11 22:08:01 +0100

    feat: add NPC entity

---
+++ Modules/NonPlayerCharacter/src/Game.NonPlayerCharacter.Domain/Data/Entities/NonPlayerCharacter.cs
@@ -0,0 +1,16 @@
+﻿using Game.NonPlayerCharacter.Domain.Data.ValueObjects;
+using Game.Shared.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.NonPlayerCharacter.Domain.Data.Entities
+{
+    public sealed class NonPlayerCharacter : Entity
+    {
+        public required string Name { get; init; }
+        public required NonPlayerCharacterStory Story { get; init; }
+    }
+}
