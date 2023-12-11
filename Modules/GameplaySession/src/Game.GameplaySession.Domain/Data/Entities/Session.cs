4fb9ac3b0791daa5f9107109ceb35bfa52011763
naomiEve <0xlunaric@gmail.com>
2023-12-11 17:52:57 +0100

    refactor: move playerlifestate to a different directory.

--- Modules/GameplaySession/src/Game.GameplaySession.Domain/Data/Entities/Session.cs
+++ Modules/GameplaySession/src/Game.GameplaySession.Domain/Data/Entities/Session.cs
@@ -1,3 +1,4 @@
+using Game.GameplaySession.Domain.Data.Enums;
 using Game.GameplaySession.Domain.Data.ValueObjects;
 using Game.Shared.Core.Data;
 
--- Modules/GameplaySession/src/Game.GameplaySession.Domain/Data/Enums/PlayerLifeState.cs
+++ Modules/GameplaySession/src/Game.GameplaySession.Domain/Data/Enums/PlayerLifeState.cs
@@ -1,7 +1,8 @@
-namespace Game.GameplaySession.Domain.Data.ValueObjects
+namespace Game.GameplaySession.Domain.Data.Enums
 {
     /// <summary>
     /// The life state of the player, either dead or alive.
+    /// TODO: This *should* map to a life state strategy class in the future, instead of being used directly.
     /// </summary>
     public enum PlayerLifeState
     {
