81cd62d867b870049f5efad16c422c8c9fbc9d27
Marcin Badurowicz <m@badurowicz.net>
2024-01-15 19:56:23 +0100

    feat: user stats on game screen

---
+++ Modules/ItemGenerator/ItemGenerator.Domain/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ItemGeneratorModels
+{
+    public class Item
+    {
+        public string Name { get; set; } = null!;
+        public string History { get; set; } = null!;
+
+        public ItemRarity Rarity { get; set; } = ItemRarity.Common;
+
+        public Dictionary<ItemStat, int> Statistics { get; init; } = new();
+
+        public void SetStat(ItemStat stat, int value)
+        {
+            Statistics[stat] = value;
+        }
+    }
+}
---
+++ Modules/ItemGenerator/ItemGenerator.Domain/StatRange.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItemGeneratorModels
+{
+    public record StatRange(int MinValue, int MaxValue)
+    {
+        public int GenerateStat(Random rnd)
+        {
+            return rnd.Next(MinValue, MaxValue + 1);
+        }
+    }
+}
