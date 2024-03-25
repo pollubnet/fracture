1d7a101c5c4b0d3ee98e9f9fc7dc01fc786c03e4
Marcin Badurowicz <m@badurowicz.net>
2024-03-25 13:03:02 +0100

    feat: include item-generator project in the main one

---
+++ Server/Modules/ItemGenerator/BogusItemGenerator.cs
@@ -0,0 +1,43 @@
+﻿using Bogus.DataSets;
+using ItemGeneratorModels;
+using ExampleItemGenerator.Services.Generators;
+using System.Text.Json;
+
+namespace ExampleItemGenerator.Api
+{
+    public class BogusItemGenerator : IItemGenerator
+    {
+        private readonly Random _rnd;
+        private readonly List<RarityModifier> _modifiers;
+        private readonly Lorem lorem;
+        private readonly Hacker hacker;
+
+        public BogusItemGenerator()
+        {
+            _rnd = new Random();
+
+            var configData = File.ReadAllText("config.json");
+            _modifiers = JsonSerializer.Deserialize<List<RarityModifier>>(configData)!;
+            lorem = new Bogus.DataSets.Lorem();
+            hacker = new Bogus.DataSets.Hacker();
+        }
+
+        public Task<Item> Generate()
+        {
+            var value = _rnd.NextSingle();
+            var modifier = _modifiers.First(m => m.ValueBelow > value);
+
+            var item = new Item
+            {
+                Name = hacker.Noun(),
+                Rarity = modifier.Rarity,
+                History = lorem.Paragraph()
+            };
+
+            foreach (var itemStat in Enum.GetValues<ItemStat>())
+                item.SetStat(itemStat, modifier.StatRanges[itemStat].GenerateStat(_rnd));
+
+            return Task.FromResult(item);
+        }
+    }
+}
---
+++ Server/Modules/ItemGenerator/ItemGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json;
+using ItemGeneratorModels;
+
+namespace ExampleItemGenerator.Services.Generators
+{
+    public class ItemGenerator : IItemGenerator
+    {
+        private readonly Random _rnd;
+        private readonly List<RarityModifier> _modifiers;
+        private readonly INameGenerator _nameGenerator;
+
+        public ItemGenerator()
+        {
+            _nameGenerator = new InternetNameGenerator();
+
+            _rnd = new Random();
+
+            var configData = File.ReadAllText("config.json");
+            _modifiers = JsonSerializer.Deserialize<List<RarityModifier>>(configData)!;
+        }
+
+        public async Task<Item> Generate()
+        {
+            var value = _rnd.NextSingle();
+            var modifier = _modifiers.First(m => m.ValueBelow > value);
+
+            var item = new Item
+            {
+                Name = await _nameGenerator.GenerateName(),
+                Rarity = modifier.Rarity
+            };
+
+            foreach (var itemStat in Enum.GetValues<ItemStat>())
+                item.SetStat(itemStat, modifier.StatRanges[itemStat].GenerateStat(_rnd));
+
+            return item;
+        }
+    }
+}
