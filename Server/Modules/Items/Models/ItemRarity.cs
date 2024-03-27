7c1d36d88c9f0b73a08f2be80bf041b082598fc3
Xylo <cezary2000@gmail.com>
2024-03-27 13:20:15 +0100

    refactor: rewrite as an extension method

--- Server/Modules/Items/Models/ItemRarity.cs
+++ Server/Modules/Items/Models/ItemRarity.cs
@@ -12,3 +12,30 @@ namespace Fracture.Server.Modules.Items.Models
         Legendary
     }
 }
+
+namespace Fracture.Server.EnumExtensions
+{
+    using Fracture.Server.Modules.Items.Models;
+
+    public static class ItemRarityExtensions
+    {
+        public static string ToCssClassName(this ItemRarity rarity)
+        {
+            return rarity switch
+            {
+                ItemRarity.Common => "--rarity-common",
+                ItemRarity.Uncommon => "--rarity-uncommon",
+                ItemRarity.Rare => "--rarity-rare",
+                ItemRarity.Insane => "--rarity-insane",
+                ItemRarity.Epic => "--rarity-epic",
+                ItemRarity.Legendary => "--rarity-legendary",
+                _
+                    => throw new ArgumentOutOfRangeException(
+                        nameof(rarity),
+                        rarity,
+                        $"Rarity '{rarity}' does not have a corresponding CSS class."
+                    )
+            };
+        }
+    }
+}
