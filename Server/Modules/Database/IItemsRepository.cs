66df69d58308c0125a7a0e87ee24f40357540960
Xylo <cezary2000@gmail.com>
2024-04-10 16:53:56 +0200

    feat: items and users are now stored in the database
    Trying to generate items while not logged in currently crashes the server. After logging in, everything works. Relogging to a different user also works.

---
+++ Server/Modules/Database/IItemsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Fracture.Server.Modules.Items.Models;
+
+namespace Fracture.Server.Modules.Database
+{
+    public interface IItemsRepository
+    {
+        public Task<Item> AddItemAsync(Item item);
+        public Task<Item?> GetItemAsync(int id);
+        public Task<ICollection<Item>> GetItemsOfUserAsync(int userId);
+        public Task<Item> UpdateItemAsync(Item item);
+    }
+}
