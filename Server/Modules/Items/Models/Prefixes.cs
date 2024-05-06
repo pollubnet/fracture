c58818920eb350c20c670042232e7c7138954717
Marcin Badurowicz <m@badurowicz.net>
2024-05-06 19:40:24 +0200

    refactor: rename things

--- Server/Modules/Items/Models/Prefixes.cs
+++ Server/Modules/Items/Models/Prefixes.cs
@@ -5,91 +5,90 @@ namespace Fracture.Server.Modules.Items.Models
 {
     public class Prefixes
     {
-        Dictionary<string, int> PerfectionNames = new Dictionary<string, int>()
-        {
-            { "Eldritch", 61 },
-            { "Efficient", 41 },
-            { "Versatile", 21 },
-            { "Balanced", 0 },
-            { "Unsteady", -20 },
-            { "Poisoned", -40 },
-            { "Toxic", -60 },
-            { "Cursed", -80 },
-            { "Demonic", -100 }
-        };
+        private static readonly Dictionary<string, int> PerfectionNames =
+            new()
+            {
+                { "Eldritch", 61 },
+                { "Efficient", 41 },
+                { "Versatile", 21 },
+                { "Balanced", 0 },
+                { "Unsteady", -20 },
+                { "Poisoned", -40 },
+                { "Toxic", -60 },
+                { "Cursed", -80 },
+                { "Demonic", -100 }
+            };
 
-        Dictionary<ItemStat, Dictionary<string, int>> StatNames = new Dictionary<
-            ItemStat,
-            Dictionary<string, int>
-        >()
-        {
+        private static readonly Dictionary<ItemStat, Dictionary<string, int>> StatNames =
+            new()
             {
-                ItemStat.Luck,
-                new Dictionary<string, int>()
                 {
-                    { "Destined", 81 },
-                    { "Predestined", 61 },
-                    { "Prosperous", 41 },
-                    { "Fortunate", 21 },
-                    { "Lucky", 0 },
-                    { "Unlucky", -20 },
-                    { "Unfortunate", -40 },
-                    { "Clumsy", -60 },
-                    { "Fateful", -80 },
-                    { "Misfortune-laden", -100 }
-                }
-            },
-            {
-                ItemStat.Health,
-                new Dictionary<string, int>()
+                    ItemStat.Luck,
+                    new Dictionary<string, int>()
+                    {
+                        { "Destined", 81 },
+                        { "Predestined", 61 },
+                        { "Prosperous", 41 },
+                        { "Fortunate", 21 },
+                        { "Lucky", 0 },
+                        { "Unlucky", -20 },
+                        { "Unfortunate", -40 },
+                        { "Clumsy", -60 },
+                        { "Fateful", -80 },
+                        { "Misfortune-laden", -100 }
+                    }
+                },
                 {
-                    { "Unbreakable", 81 },
-                    { "Vital", 61 },
-                    { "Sturdy", 41 },
-                    { "Durable", 21 },
-                    { "Stable", 0 },
-                    { "Delicate", -20 },
-                    { "Feeble", -40 },
-                    { "Flimsy", -60 },
-                    { "Fragile", -80 },
-                    { "Ethereal", -100 }
-                }
-            },
-            {
-                ItemStat.Armor,
-                new Dictionary<string, int>()
+                    ItemStat.Health,
+                    new Dictionary<string, int>()
+                    {
+                        { "Unbreakable", 81 },
+                        { "Vital", 61 },
+                        { "Sturdy", 41 },
+                        { "Durable", 21 },
+                        { "Stable", 0 },
+                        { "Delicate", -20 },
+                        { "Feeble", -40 },
+                        { "Flimsy", -60 },
+                        { "Fragile", -80 },
+                        { "Ethereal", -100 }
+                    }
+                },
                 {
-                    { "Specialized", 81 },
-                    { "Reliable", 61 },
-                    { "Reinforced", 41 },
-                    { "Bulky", 21 },
-                    { "Simple", 0 },
-                    { "Crude", -20 },
-                    { "Plain", -40 },
-                    { "Rusty", -60 },
-                    { "Unreliable", -80 },
-                    { "Primitive", -100 }
-                }
-            },
-            {
-                ItemStat.Power,
-                new Dictionary<string, int>()
+                    ItemStat.Armor,
+                    new Dictionary<string, int>()
+                    {
+                        { "Specialized", 81 },
+                        { "Reliable", 61 },
+                        { "Reinforced", 41 },
+                        { "Bulky", 21 },
+                        { "Simple", 0 },
+                        { "Crude", -20 },
+                        { "Plain", -40 },
+                        { "Rusty", -60 },
+                        { "Unreliable", -80 },
+                        { "Primitive", -100 }
+                    }
+                },
                 {
-                    { "Lethal", 81 },
-                    { "Brutal", 61 },
-                    { "Sharp", 41 },
-                    { "Precise", 21 },
-                    { "Reasonable", 0 },
-                    { "Blunt", -20 },
-                    { "Dull", -40 },
-                    { "Brittle", -60 },
-                    { "Weak", -80 },
-                    { "Wasteful", -100 }
-                }
-            },
-        };
+                    ItemStat.Power,
+                    new Dictionary<string, int>()
+                    {
+                        { "Lethal", 81 },
+                        { "Brutal", 61 },
+                        { "Sharp", 41 },
+                        { "Precise", 21 },
+                        { "Reasonable", 0 },
+                        { "Blunt", -20 },
+                        { "Dull", -40 },
+                        { "Brittle", -60 },
+                        { "Weak", -80 },
+                        { "Wasteful", -100 }
+                    }
+                },
+            };
 
-        private void addPerfectionPrefix(Item item, float statsAverage)
+        private void AddPerfectionPrefix(Item item, float statsAverage)
         {
             foreach (var perfectionIteration in PerfectionNames)
             {
@@ -101,7 +100,7 @@ namespace Fracture.Server.Modules.Items.Models
             }
         }
 
-        private ItemStat findMaxAbsoluteStat(Item item)
+        private ItemStat FindMaxAbsoluteStat(Item item)
         {
             int maxAbsoluteStatValue = -1;
             ItemStat maxAbsoluteStatName = ItemStat.Luck;
@@ -118,25 +117,25 @@ namespace Fracture.Server.Modules.Items.Models
             return maxAbsoluteStatName;
         }
 
-        private void addStatPrefix(Item item, ItemStat stat)
+        private void AddStatPrefix(Item item, ItemStat stat)
         {
             // TODO: findStatPrefxi(item, stat)
 
             if (stat == ItemStat.Luck)
             {
-                findStatPrefix(item, ItemStat.Luck);
+                FindStatPrefix(item, ItemStat.Luck);
             }
             else if (stat == ItemStat.Health)
             {
-                findStatPrefix(item, ItemStat.Health);
+                FindStatPrefix(item, ItemStat.Health);
             }
             else if (stat == ItemStat.Armor)
             {
-                findStatPrefix(item, ItemStat.Armor);
+                FindStatPrefix(item, ItemStat.Armor);
             }
             else if (stat == ItemStat.Power)
             {
-                findStatPrefix(item, ItemStat.Power);
+                FindStatPrefix(item, ItemStat.Power);
             }
             else
             {
@@ -144,7 +143,7 @@ namespace Fracture.Server.Modules.Items.Models
             }
         }
 
-        private void findStatPrefix(Item item, ItemStat itemStat)
+        private void FindStatPrefix(Item item, ItemStat itemStat)
         {
             int statValue = item.Statistics.GetStatFromItemStat(itemStat);
             foreach (var iteration in StatNames[itemStat])
@@ -157,7 +156,7 @@ namespace Fracture.Server.Modules.Items.Models
             }
         }
 
-        public void addPrefixes(Item item)
+        public void AddPrefixes(Item item)
         {
             int totalStats = Enum.GetValues<ItemStat>()
                 .Aggregate(0, (acc, y) => acc + item.Statistics.GetStatFromItemStat(y));
@@ -168,9 +167,9 @@ namespace Fracture.Server.Modules.Items.Models
                 // TODO: special name generator
                 return;
             }
-            ItemStat stat = findMaxAbsoluteStat(item);
-            addStatPrefix(item, stat);
-            addPerfectionPrefix(item, statsAverage);
+            ItemStat stat = FindMaxAbsoluteStat(item);
+            AddStatPrefix(item, stat);
+            AddPerfectionPrefix(item, statsAverage);
         }
     }
 }
