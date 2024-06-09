dc2a526be8dd4944159c004467f0a97f4695e468
Marcin Badurowicz <m@badurowicz.net>
2024-06-09 23:37:58 +0200

    feat: add OpenAI-compatible API support

---
+++ Server/Modules/AI/Models/AIBackendConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace Fracture.Server.Modules.AI.Models;
+
+/// <summary>
+/// The config for the AI provider's endpoint.
+/// </summary>
+public class AIBackendConfiguration
+{
+    /// <summary>
+    /// The OpenAI-compatible endpoint's URL. If null, the default one (OpenAI)
+    /// will be used.
+    /// </summary>
+    public string? EndpointUrl { get; set; }
+
+    /// <summary>
+    /// The API key to be used during the communication with the AI backend.
+    /// </summary>
+    public string? ApiKey { get; set; }
+
+    /// <summary>
+    /// Name of the model to be used (e.g. "chatgpt-3.5-turbo" or "mistral")
+    /// </summary>
+    public required string Model { get; set; }
+}
---
+++ Server/Modules/AI/Services/OpenAICompatibleInstructionProvider.cs
@@ -0,0 +1,59 @@
+﻿using Fracture.Server.Modules.AI.Models;
+using Microsoft.Extensions.Options;
+using OpenAI;
+using OpenAI.Chat;
+
+namespace Fracture.Server.Modules.AI.Services;
+
+public class OpenAICompatibleInstructionProvider : IAIInstructionProvider
+{
+    private readonly OpenAIClient _api;
+    private readonly AIBackendConfiguration _configuration;
+
+    public OpenAICompatibleInstructionProvider(IOptions<AIBackendConfiguration> configuration)
+    {
+        _configuration = configuration.Value;
+
+        if (_configuration.EndpointUrl is null)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(nameof(_configuration.ApiKey));
+
+            _api = new OpenAIClient(new OpenAIAuthentication(_configuration.ApiKey));
+        }
+        else
+        {
+            var settings = new OpenAIClientSettings(domain: _configuration.EndpointUrl);
+
+            if (_configuration.ApiKey is null)
+            {
+                _api = new OpenAIClient(clientSettings: settings);
+            }
+            else
+            {
+                _api = new OpenAIClient(new OpenAIAuthentication(_configuration.ApiKey), settings);
+            }
+        }
+    }
+
+    public async Task<string> GenerateInstructionResponse(string instruction)
+    {
+        return await GenerateResponse(new() { Prompt = instruction });
+    }
+
+    public async Task<string> GenerateResponse(AIGenerationContext context)
+    {
+        var messages = new List<Message> { new Message(Role.User, context.Prompt) };
+
+        var chatRequest = new ChatRequest(
+            messages,
+            model: context.Model ?? _configuration.Model,
+            temperature: context.Temperature,
+            stops: context.StopTokens,
+            maxTokens: context.MaxTokens,
+            topP: context.TopP
+        );
+        var response = await _api.ChatEndpoint.GetCompletionAsync(chatRequest);
+
+        return response.FirstChoice;
+    }
+}
