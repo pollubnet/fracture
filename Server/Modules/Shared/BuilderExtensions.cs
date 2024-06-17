42c2b51ca5727c08172d175d02aaa5136d780506
Marcin Badurowicz <m@badurowicz.net>
2024-06-17 11:47:31 +0200

    feat: register services only if feature flags are enabled

---
+++ Server/Modules/Shared/BuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Fracture.Server.Modules.AI.Services;
+using Fracture.Server.Modules.Shared.Configuration;
+using Microsoft.FeatureManagement;
+
+namespace Fracture.Server.Modules.Shared
+{
+    public static class BuilderExtensions
+    {
+        /// <summary>
+        /// Adds to services elements only if there is a proper feature flag enabled in the configuration
+        /// </summary>
+        public static async Task AddFeatureGatedServices(this IServiceCollection services)
+        {
+            var serviceProvider = services.BuildServiceProvider();
+            var featureManager = serviceProvider.GetRequiredService<IFeatureManager>();
+            if (await featureManager.IsEnabledAsync(FeatureFlags.USE_AI))
+            {
+                services.AddSingleton<
+                    IAIInstructionProvider,
+                    OpenAICompatibleInstructionProvider
+                >();
+            }
+        }
+    }
+}
