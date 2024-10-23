4641932011f6efc30a5e755b97558694b588d8be
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-10-24 01:15:25 +0200

    fix: switch to new perlin generation (IT WORKS)

--- Server/Modules/MapGenerator/Services/MapGeneratorService.cs
+++ Server/Modules/MapGenerator/Services/MapGeneratorService.cs
@@ -11,8 +11,8 @@ public class MapGeneratorService : IMapGeneratorService
 
     private readonly float _persistence = 0.5f;
     private readonly float _lacunarity = 2f;
-    private readonly int _octaves = 5;
-    private readonly float _scale = 5f;
+    private readonly int _octaves = 3;
+    private readonly float _scale = 10f;
 
     private Random _rnd = new Random();
 
@@ -31,24 +31,42 @@ public class MapGeneratorService : IMapGeneratorService
 
     private MapData GenerateMap(NoiseParameters noiseParameters)
     {
-        int width = 32;
-        int height = 32;
-        bool useFalloff = true;
+        int width = 128;
+        int height = 128;
+        bool useFalloff = false;
         _seed = noiseParameters.UseRandomSeed ? _rnd.Next(int.MaxValue) : noiseParameters.Seed;
 
         var grid = new Node[width, height];
 
         var falloffMap = FalloffGenerator.Generate(width);
-        var heightMap = PerlinNoiseGenerator.Generate(
+        // var heightMap = PerlinNoiseGenerator.Generate(
+        //     width,
+        //     height,
+        //     _seed,
+        //     _scale,
+        //     _octaves,
+        //     _persistence,
+        //     _lacunarity,
+        //     Vector2.Zero
+        // );
+
+
+        var heightMap = CustomPerlin.GenerateNoiseMap(
             width,
-            height,
             _seed,
-            _scale,
             _octaves,
             _persistence,
             _lacunarity,
-            Vector2.Zero
+            _scale
         );
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                heightMap[i, j] = Math.Clamp(0.3f + heightMap[i, j] * 0.8f, 0, 1);
+            }
+        }
 
         var temperatureMap = PerlinNoiseGenerator.Generate(
             width,
