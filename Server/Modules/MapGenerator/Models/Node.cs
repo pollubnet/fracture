dae37f1d9a258206fe7686f2b7b84fa316c0bad7
Kamil Ziolkowski <kamilziolkowski231@gmail.com>
2024-11-26 23:23:06 +0100

    chore: singleton BiomeFactory, adding biomes into nodes in map grid

--- Server/Modules/MapGenerator/Models/Node.cs
+++ Server/Modules/MapGenerator/Models/Node.cs
@@ -1,23 +1,28 @@
-﻿using Fracture.Server.Modules.Pathfinding.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Fracture.Server.Modules.Pathfinding.Models;
 
 namespace Fracture.Server.Modules.MapGenerator.Models;
 
 public class Node : IPathfindingNode
 {
+    [Required]
     public int XId { get; set; }
+
+    [Required]
     public int YId { get; set; }
-    public Biome? Biome { get; set; }
 
-    public string? Color { get; set; }
+    [Required]
+    public Biome Biome { get; set; }
+
+    [Required]
     public float NoiseValue { get; set; }
-
     public int GCost { get; set; }
     public int HCost { get; set; }
     public int FCost => GCost + HCost;
     public bool Walkable { get; set; }
     public IPathfindingNode? PreviousNode { get; set; }
 
-    public Node(int xId, int yId, Biome? biome)
+    public Node(int xId, int yId, Biome biome)
     {
         XId = xId;
         YId = yId;
