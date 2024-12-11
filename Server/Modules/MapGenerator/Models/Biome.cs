2c8219e538d4fa51d3c58d60bc3d200d3fcb71de
Kamil Ziolkowski <kamilziolkowski231@gmail.com>
2024-12-12 00:13:29 +0100

    del: useless class

--- Server/Modules/MapGenerator/Models/Biome.cs
+++ Server/Modules/MapGenerator/Models/Biome.cs
@@ -6,8 +6,6 @@ namespace Fracture.Server.Modules.MapGenerator.Models;
 public class Biome : BiomeCategory
 {
     public string? Name { get; set; }
-
-    [JsonConverter(typeof(ColorJsonConverter))]
     public Color Color { get; set; }
     public float MinTemperature { get; set; }
     public float MaxTemperature { get; set; }
