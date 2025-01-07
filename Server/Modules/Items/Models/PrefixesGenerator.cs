8b10e39c68a8c0897081eb8513eac8b54d2da674
Marcin Badurowicz <m@badurowicz.net>
2025-01-07 18:51:58 +0100

    chore: fix formatting

--- Server/Modules/Items/Models/PrefixesGenerator.cs
+++ Server/Modules/Items/Models/PrefixesGenerator.cs
@@ -5,88 +5,86 @@ namespace Fracture.Server.Modules.Items.Models
 {
     public class PrefixesGenerator
     {
-        private static readonly Dictionary<string, int> PerfectionNames =
-            new()
-            {
-                { "Eldritch", 61 },
-                { "Efficient", 41 },
-                { "Versatile", 21 },
-                { "Balanced", 0 },
-                { "Unsteady", -20 },
-                { "Poisoned", -40 },
-                { "Toxic", -60 },
-                { "Cursed", -80 },
-                { "Demonic", -100 },
-            };
+        private static readonly Dictionary<string, int> PerfectionNames = new()
+        {
+            { "Eldritch", 61 },
+            { "Efficient", 41 },
+            { "Versatile", 21 },
+            { "Balanced", 0 },
+            { "Unsteady", -20 },
+            { "Poisoned", -40 },
+            { "Toxic", -60 },
+            { "Cursed", -80 },
+            { "Demonic", -100 },
+        };
 
-        private static readonly Dictionary<ItemStat, Dictionary<string, int>> StatNames =
-            new()
+        private static readonly Dictionary<ItemStat, Dictionary<string, int>> StatNames = new()
+        {
             {
+                ItemStat.Luck,
+                new()
                 {
-                    ItemStat.Luck,
-                    new()
-                    {
-                        { "Destined", 81 },
-                        { "Predestined", 61 },
-                        { "Prosperous", 41 },
-                        { "Fortunate", 21 },
-                        { "Lucky", 0 },
-                        { "Unlucky", -20 },
-                        { "Unfortunate", -40 },
-                        { "Clumsy", -60 },
-                        { "Fateful", -80 },
-                        { "Misfortune-laden", -100 },
-                    }
-                },
+                    { "Destined", 81 },
+                    { "Predestined", 61 },
+                    { "Prosperous", 41 },
+                    { "Fortunate", 21 },
+                    { "Lucky", 0 },
+                    { "Unlucky", -20 },
+                    { "Unfortunate", -40 },
+                    { "Clumsy", -60 },
+                    { "Fateful", -80 },
+                    { "Misfortune-laden", -100 },
+                }
+            },
+            {
+                ItemStat.Health,
+                new()
                 {
-                    ItemStat.Health,
-                    new()
-                    {
-                        { "Unbreakable", 81 },
-                        { "Vital", 61 },
-                        { "Sturdy", 41 },
-                        { "Durable", 21 },
-                        { "Stable", 0 },
-                        { "Delicate", -20 },
-                        { "Feeble", -40 },
-                        { "Flimsy", -60 },
-                        { "Fragile", -80 },
-                        { "Ethereal", -100 },
-                    }
-                },
+                    { "Unbreakable", 81 },
+                    { "Vital", 61 },
+                    { "Sturdy", 41 },
+                    { "Durable", 21 },
+                    { "Stable", 0 },
+                    { "Delicate", -20 },
+                    { "Feeble", -40 },
+                    { "Flimsy", -60 },
+                    { "Fragile", -80 },
+                    { "Ethereal", -100 },
+                }
+            },
+            {
+                ItemStat.Armor,
+                new()
                 {
-                    ItemStat.Armor,
-                    new()
-                    {
-                        { "Specialized", 81 },
-                        { "Reliable", 61 },
-                        { "Reinforced", 41 },
-                        { "Bulky", 21 },
-                        { "Simple", 0 },
-                        { "Crude", -20 },
-                        { "Plain", -40 },
-                        { "Rusty", -60 },
-                        { "Unreliable", -80 },
-                        { "Primitive", -100 },
-                    }
-                },
+                    { "Specialized", 81 },
+                    { "Reliable", 61 },
+                    { "Reinforced", 41 },
+                    { "Bulky", 21 },
+                    { "Simple", 0 },
+                    { "Crude", -20 },
+                    { "Plain", -40 },
+                    { "Rusty", -60 },
+                    { "Unreliable", -80 },
+                    { "Primitive", -100 },
+                }
+            },
+            {
+                ItemStat.Power,
+                new()
                 {
-                    ItemStat.Power,
-                    new()
-                    {
-                        { "Lethal", 81 },
-                        { "Brutal", 61 },
-                        { "Sharp", 41 },
-                        { "Precise", 21 },
-                        { "Reasonable", 0 },
-                        { "Blunt", -20 },
-                        { "Dull", -40 },
-                        { "Brittle", -60 },
-                        { "Weak", -80 },
-                        { "Wasteful", -100 },
-                    }
-                },
-            };
+                    { "Lethal", 81 },
+                    { "Brutal", 61 },
+                    { "Sharp", 41 },
+                    { "Precise", 21 },
+                    { "Reasonable", 0 },
+                    { "Blunt", -20 },
+                    { "Dull", -40 },
+                    { "Brittle", -60 },
+                    { "Weak", -80 },
+                    { "Wasteful", -100 },
+                }
+            },
+        };
 
         private readonly INameGenerator _nameGenerator;
 
