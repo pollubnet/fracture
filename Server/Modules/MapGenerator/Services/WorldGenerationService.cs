37c81047fabe2b094741c5c8697f68c3342582bd
Marcin Badurowicz <m@badurowicz.net>
2026-03-13 15:27:05 +0100

    refactor: add required

--- Server/Modules/MapGenerator/Services/WorldGenerationService.cs
+++ Server/Modules/MapGenerator/Services/WorldGenerationService.cs
@@ -1,39 +1,22 @@
-﻿using Fracture.Server.Modules.FloodFill;
-using Fracture.Server.Modules.MapGenerator.Models.Map;
-using Fracture.Server.Modules.MapGenerator.Models.Map.Biome;
-using Fracture.Server.Modules.MapGenerator.Models.Map.MapObjects;
-using Fracture.Server.Modules.MapGenerator.Models.Map.Town;
-using Fracture.Server.Modules.MapGenerator.Services.TownGen;
+﻿using Fracture.Server.Modules.MapGenerator.Models.Map;
 
 namespace Fracture.Server.Modules.MapGenerator.Services;
 
-public class WorldGenerationService : IWorldGenerationService
+public class WorldGenerationService(
+    IMapParameterSelectorService parameterSelector,
+    IMapGeneratorService mapGenerator,
+    ISubMapAssignmentService subMapAssigner,
+    ILogger<WorldGenerationService> logger
+) : IWorldGenerationService
 {
-    private readonly IMapParameterSelectorService _parameterSelector;
-    private readonly IMapGeneratorService _mapGenerator;
-    private readonly ISubMapAssignmentService _subMapAssigner;
-    private readonly ILogger<WorldGenerationService> _logger;
-
-    public WorldGenerationService(
-        IMapParameterSelectorService parameterSelector,
-        IMapGeneratorService mapGenerator,
-        ISubMapAssignmentService subMapAssigner,
-        ILogger<WorldGenerationService> logger
-    )
-    {
-        _parameterSelector = parameterSelector;
-        _mapGenerator = mapGenerator;
-        _subMapAssigner = subMapAssigner;
-        _logger = logger;
-    }
-
     public async Task<Map> GenerateWorldMapAsync()
     {
-        var parameters = await _parameterSelector.GetParametersAsync();
-        var mainParameter = _parameterSelector.SelectRandom(parameters, LocationType.MainLocation);
-        var map = await _mapGenerator.GetMap(mainParameter);
+        logger.LogInformation("Generating main world...");
+        var parameters = await parameterSelector.GetParametersAsync();
+        var mainParameter = parameterSelector.SelectRandom(parameters, LocationType.MainLocation);
+        var map = await mapGenerator.GetMap(mainParameter);
 
-        await _subMapAssigner.AssignSubMapsAsync(map, parameters);
+        await subMapAssigner.AssignSubMapsAsync(map, parameters);
 
         return map;
     }
