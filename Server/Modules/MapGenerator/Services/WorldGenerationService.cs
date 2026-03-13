00d6a0975fab0f07b961c03d4bf7cfc95ec6d699
Marcin Badurowicz <m@badurowicz.net>
2026-03-13 15:57:23 +0100

    feat: add naming towns

--- Server/Modules/MapGenerator/Services/WorldGenerationService.cs
+++ Server/Modules/MapGenerator/Services/WorldGenerationService.cs
@@ -1,4 +1,5 @@
 ﻿using Fracture.Server.Modules.MapGenerator.Models.Map;
+using Fracture.Server.Modules.Shared;
 
 namespace Fracture.Server.Modules.MapGenerator.Services;
 
@@ -6,6 +7,7 @@ public class WorldGenerationService(
     IMapParameterSelectorService parameterSelector,
     IMapGeneratorService mapGenerator,
     ISubMapAssignmentService subMapAssigner,
+    INameGenerator nameGenerator,
     ILogger<WorldGenerationService> logger
 ) : IWorldGenerationService
 {
@@ -13,10 +15,30 @@ public class WorldGenerationService(
     {
         logger.LogInformation("Generating main world...");
         var parameters = await parameterSelector.GetParametersAsync();
+
+        // generate main map
         var mainParameter = parameterSelector.SelectRandom(parameters, LocationType.MainLocation);
         var map = await mapGenerator.GetMap(mainParameter);
 
+        // generate submaps
         await subMapAssigner.AssignSubMapsAsync(map, parameters);
+
+        // rename locations
+        foreach (var item in map.LocationGroups)
+        {
+            item.GroupName = item.LocationType switch
+            {
+                LocationType.MainLocation => "The World",
+                LocationType.Town => "Town " + await nameGenerator.GenerateNameAsync(),
+                LocationType.Cave => "Cave " + await nameGenerator.GenerateNameAsync(),
+                _ => item.GroupName,
+            };
+
+            foreach (var node in item.Nodes)
+            {
+                node.GroupName = item.GroupName;
+            }
+        }
 
         return map;
     }
