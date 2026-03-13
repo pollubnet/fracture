4b944014e2de5f4b22d56e01cb0d25be7bbf1a8d
Marcin Badurowicz <m@badurowicz.net>
2026-03-13 13:39:48 +0100

    chore: remove unused services

--- Server/Program.cs
+++ Server/Program.cs
@@ -101,8 +101,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     app.UseWebAssemblyDebugging();
 }
 else
