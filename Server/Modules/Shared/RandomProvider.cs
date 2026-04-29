0a23e387dcd2cec3f4bdc886101eac72bb9bd00b
Marcin Badurowicz <m@badurowicz.net>
2026-04-29 15:59:17 +0200

    refactor: single seed for item generation

---
+++ Server/Modules/Shared/RandomProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace Fracture.Server.Modules.Shared
+{
+    public class RandomProvider
+    {
+        public Random Random { get; private set; }
+
+        public RandomProvider(int seed)
+        {
+            Random = new Random(seed);
+        }
+    }
+}
