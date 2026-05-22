cf476f2914a986f3ee22fefa81cf6696040986b6
Mateusz Poświata <mateusz147@vp.pl>
2026-05-22 20:10:17 +0200

    Add version info and move 'Generate new item' button to gear menu

--- Server/Components/Popups/EquipmentPopup.razor.cs
+++ Server/Components/Popups/EquipmentPopup.razor.cs
@@ -9,20 +9,6 @@ public partial class EquipmentPopup
 {
     protected override async Task OnInitializedAsync() { }
 
-    public async Task GenerateNewItem()
-    {
-        var item = await ItemGenerator.Generate();
-
-        if (item is not null)
-        {
-            item.CreatedBy = UserService.User!;
-            item.CreatedById = UserService.User!.Id;
-
-            await ItemsRepository.AddItemAsync(item);
-            UserService.Inventory.Add(item);
-        }
-    }
-
     public void Equip(Item item)
     {
         UserService.Equip(item);
