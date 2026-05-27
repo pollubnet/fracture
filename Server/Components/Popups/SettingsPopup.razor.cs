fb5e0f9a5631b9d79ed5ba5643829750858c0573
metix3255 <48591062+metix3255@users.noreply.github.com>
2026-05-27 15:36:46 +0200

    feat: add version info and move 'Generate new item' button to gear menu (#112)

---
+++ Server/Components/Popups/SettingsPopup.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace Fracture.Server.Components.Popups;
+
+public partial class SettingsPopup
+{
+    public async Task GenerateNewItem()
+    {
+        var item = await ItemGenerator.Generate();
+
+        if (item is not null)
+        {
+            item.CreatedBy = UserService.User!;
+            item.CreatedById = UserService.User!.Id;
+
+            await ItemsRepository.AddItemAsync(item);
+            UserService.Inventory.Add(item);
+        }
+    }
+}
